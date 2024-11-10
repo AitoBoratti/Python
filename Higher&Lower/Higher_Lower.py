0017a6ade2b45534a215e9e0d3bbc392e9fbe46f
Alberto Boratti <albertoboratti@gmail.com>
2024-11-10 12:52:43 -0300

    Updating Higher and Lower game.

--- Higher&Lower/Higher_Lower.py
+++ Higher&Lower/Higher_Lower.py
@@ -6,6 +6,7 @@ class Higher_Lower():
     QUESTION = "Who has more followers? Type 'A' or 'B':"
     FAIL_STR = "Sorry, that's wrong. Final score:"
     CORRECT_STR = "You're right! Current score:"
+    CONGRATULATION_STR = "Congratulations, you have managed to answer all the questions correctly! Have a cookie!"
     WARNING_STR = """               
                     ¡¡¡WARNING!!!
               ¡Only A or B is accepted! 
@@ -17,54 +18,64 @@ class Higher_Lower():
     def __init__(self):
         self.score = 0
         self.game_data = d.data
-        self.first_data = self.get_random_data()
+        self.first_data = self._get_random_data()
         self.second_data = None
 
 
-    def print_logo(self):
+    def _print_logo(self):
         sys("cls")
         print(d.logo)
 
 
-    def print_fail(self):
-        self.print_logo()
+    def _print_fail(self):
+        self._print_logo()
         print(f"{self.FAIL_STR} {self.score}")
 
 
-    def print_warning(self):
+    def _print_flawless_victory(self):
+        self._print_logo()
+        print(self.CONGRATULATION_STR)
+        print(d.cookie)
+
+
+    def _print_warning(self):
         sys("cls")
         input(self.WARNING_STR)
 
 
-    def display_data(self, data, label):
+    def _get_random_data(self):
+        return self.game_data.pop(r.randint(0, len(self.game_data) - 1))
+ 
+ 
+    def _display_data(self, data, label):
         print(f"{label}: {data['name']}, a {data['description']}, from {data['country']}.")
 
 
     def print_stage(self, win=False):
-        self.print_logo()
+        self._print_logo()
         if win:
             print(f"{self.CORRECT_STR} {self.score}")
-        self.display_data(self.first_data, "Compare A")
+        self._display_data(self.first_data, "Compare A")
         print(d.vs)
-        self.display_data(self.second_data, "Against B")
+        self._display_data(self.second_data, "Against B")
 
 
-    def get_random_data(self):
-        return self.game_data.pop(r.randint(0, len(self.game_data) - 1))
-
-
+    def _have_data(self):
+        return (len(self.game_data) != 0)
+    
+    
     def check_answer(self):
         res = input(self.QUESTION).capitalize()
         if res not in ['A', 'B']:
-            self.print_warning()
+            self._print_warning()
             return None
         return ((res == "A" and self.first_data["follower_count"] > self.second_data["follower_count"]) or
                 (res == "B" and self.first_data["follower_count"] < self.second_data["follower_count"]))
 
 
     def play(self):
-        while True:
-            self.second_data = self.get_random_data()
+        while self._have_data():
+            self.second_data = self._get_random_data()
             self.print_stage(bool(self.score))
             
             correct = self.check_answer()
@@ -73,90 +84,14 @@ class Higher_Lower():
                 self.first_data = self.second_data
                 self.score += 1
             elif correct is None:
-                self.first_data = self.get_random_data()
+                self.first_data = self._get_random_data()
             else:
+                self._print_fail()
                 break
-        self.print_fail()
+            
+        if not self._have_data():
+            self._print_flawless_victory()
 
-
+# Test
 game = Higher_Lower()
 game.play()
-
-
-
-
-
-# ----------------------------------- Codigo Pre Refactorizacion -----------------------------------------
-
-
-
-# score = 0
-# game_data = d.data
-# QUESTION = "Who has more followers? Type \'A\' or \'B\':"
-# FAIL_STR = f"Sorry, that's wrong. Final score:"
-# CORRECT_STR = "You're right! Current score:"
-# WARNING_STR = """               
-#                 ¡¡¡WARNING!!!
-#           ¡Only A or B is accepted! 
-          
-#          Click any key to continue...
-# """
-
-
-# def _print_logo():
-#     sys("cls")
-#     print(d.logo)
-
-
-# def print_fail():
-#     _print_logo()
-#     print(f"{FAIL_STR} {score}")
-
-
-# def print_warning():
-#     sys("cls")
-#     input(WARNING_STR)
-
-
-# def _display_data(data,label):
-#     print(f"{label}: {data["name"]}, a {data["description"]}, from {data["country"]}.")
-
-
-# def print_stage(win=False):
-#     _print_logo()
-#     if win:
-#         print(f"{CORRECT_STR} {score}") 
-#     _display_data(first_data,"Compare A")
-#     print(d.vs)
-#     _display_data(second_data,"Against B")
-
-
-# def get_random_data():
-#     return game_data.pop(r.randint(0,len(game_data)-1))
-
-
-# def check_answer():
-#     res = input(QUESTION).capitalize()
-#     if res not in['A','B']:
-#         print_warning()
-#         return None
-#     return (((res == "A") and first_data["follower_count"] > second_data["follower_count"]) or 
-#             ((res == "B") and first_data["follower_count"] < second_data["follower_count"]))
-
-
-# first_data = get_random_data()
-# while True:
-#     second_data = get_random_data()
-#     print_stage(bool(score))     
-#     correct = check_answer() 
-
-#     if correct:
-#         first_data = second_data 
-#         score += 1
-
-#     elif correct is None:
-#         first_data = get_random_data()
-
-#     else:
-#         break
-# print_fail()        
